61b2dc72c6172cfa79bce6cc2328a6af11377bfc
tharunsi Name <tharunsitharunsi@gmail.com>
2025-04-14 13:48:28 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,16 @@
 
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import Quizlanding from './Pages/Quizlanding/Quizlanding'
+import Landing from './Pages/Landing/Landing'
+import Leaderboard from './Pages/Leaderboard/Leaderboard'
+import Signup from './Pages/Signup/Signup'
+import Login from './Pages/Login/Login'
+import Result from './Pages/Result/Result'
+import Profile from './Pages/Profile/Profile'
 
 
-// import ChatMessage from './Pages/Chatbot/ChatMessage' use this for reference
 function App() {
   
 
@@ -12,11 +19,16 @@ function App() {
     <div>
     
     <Routes>
-     <Route path="/home" element={<Home />} />
+    
+     <Route path="/" element={<Landing/>} />
+     <Route path="/signup" element={<Signup />} /> 
+     <Route path="/login" element={<Login />} /> 
      <Route path="/dashboard" element={<Dashboard />} /> 
      <Route path="/quiz-landing" element={<Quizlanding />} /> 
+     <Route path="/leaderboard" element={<Leaderboard />} /> 
+     <Route path="/result" element={<Result />} /> 
+     <Route path="/profile" element={<Profile />} /> 
      
-     <Route path="/" element={<Landing/>} />
 
     
     </Routes>
---
+++ client/src/Pages/Quizlanding/Quizlanding.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quizlanding = () => {
+  return (
+    <div>
+      <h1>ql</h1>
+    </div>
+  )
+}
+
+export default Quizlanding
